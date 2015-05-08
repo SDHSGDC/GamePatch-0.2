eb7cdc4270e58b88f303e514fcb91f87130586cd
johnsonnikolaus <johnsonnikolaus@gmail.com>
2015-05-07 22:02:11 -0700

    The Latest Stuff

---
+++ Scripts/IntroMessage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroMessage : MonoBehaviour {
+
+	public int timeToShow;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(DeleteUI (timeToShow));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator DeleteUI(float seconds){
+//		Debug.Log ("Coroutine Started");
+		yield return new WaitForSeconds (seconds);
+		Destroy (gameObject);
+//		Debug.Log ("Coroutine Ended");
+	}
+}
---
+++ Scripts/PipeTransporter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PipeTransporter : MonoBehaviour {
+
+	public int effectiveDistance = 0;
+	public int desiredLevel = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.DrawRay (this.transform.position, Vector3.down);
+
+		Ray pipeRay = new Ray (this.transform.position, Vector3.down);
+		RaycastHit hit;
+
+		if(Physics.Raycast(pipeRay, out hit, effectiveDistance)) {
+
+			if(hit.transform.tag == "Player"){
+				Debug.Log ("Hit Player");
+//				Application.LoadLevel(desiredLevel);
+				hit.transform.Translate(0,1,0);
+			}
+
+		}
+	
+	}
+
+}
